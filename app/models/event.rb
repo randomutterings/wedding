da8fb2dc2762e427a6e2d2af09dfcd4f978359cf
Chris Barnes <randomutterings@gmail.com>
2010-02-28 20:03:16 -0500

    fixed bug where date wasn't set the way the db likes it and auto populated the scheduled_string for Chronc on edits

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,25 @@ class Event < ActiveRecord::Base
   attr_accessible :title, :description, :scheduled_at, :scheduled_string
   attr_accessor :scheduled_string
   
-  validates_presence_of :title
-  
+  validates_presence_of :title  
+
   def validate
     if date = Chronic.parse(self.scheduled_string)
-      self.scheduled_at = date
+      self.scheduled_at = date.to_s(:db)
     else
       self.errors.add :scheduled_string, "was not a proper date"
     end
   end
   
-  def scheduled_at_formatted(time = true, date = true)
-    if time == true && date == true
+  def scheduled_at_formatted(format = "datetime")
+    if format == "datetime"
       scheduled_at.strftime "%a %b #{scheduled_at.day.ordinalize}, at %l:%M %p"
-    elsif time == false && date == true
+    elsif format == "date"
       scheduled_at.strftime "%A %B #{scheduled_at.day.ordinalize}"
-    elsif time == true && date == false
-      scheduled_at.strftime "%l:%M %p"  
+    elsif format == "time"
+      scheduled_at.strftime "%l:%M %p"
+    elsif format == "chronic"
+      scheduled_at.strftime "%b %d at %l:%M %p"
     end
   end
   
