73ab6c82eaf3abb3ba8bd56220849f67a7939835
Chris Barnes <randomutterings@gmail.com>
2010-03-04 19:23:24 -0500

    updated routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :members, :as => 'party'
 
-  map.static ':permalink', :controller => 'pages', :action => 'show'
+  map.page_permalink ':permalink', :controller => 'pages', :action => 'show'
   
   map.root :controller => 'pages', :action => 'show', :permalink => 'home'
 end
