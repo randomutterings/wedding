765ac6168517ac5a2d203c3e02e072ecf450a01d
Chris Barnes <randomutterings@gmail.com>
2010-03-12 22:49:26 -0500

    added runtime error for missing config.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,6 @@
 config_file = "#{RAILS_ROOT}/config/config.yml"
-APP_CONFIG = YAML.load_file(config_file) if FileTest.exists?(config_file)
+if FileTest.exists?(config_file)
+  APP_CONFIG = YAML.load_file(config_file)
+else
+  raise "Can't find config.yml"
+end
