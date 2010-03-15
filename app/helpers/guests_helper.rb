ba6d2591087d23e2a1b66a4676784164fcf365fc
Chris Barnes <randomutterings@gmail.com>
2010-03-15 13:25:10 -0400

    updated rsvp to use jquery for nested attributes

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,22 +1,21 @@
 module GuestsHelper
-  def remove_link_unless_new_record(fields, name)
-    out = ''
-    out << fields.hidden_field(:_delete)  unless fields.object.new_record?
-    out << link_to("Remove #{name}", "##{fields.object.class.name.underscore}", :class => 'remove')
-    out
+  def remove_child_link(name, f)
+    f.hidden_field(:_delete) + link_to(name, "javascript:void(0)", :class => "remove_child")
   end
-
-  def generate_html(form_builder, method, options = {})
-    options[:object] ||= form_builder.object.class.reflect_on_association(method).klass.new
-    options[:partial] ||= method.to_s.singularize
-    options[:form_builder_local] ||= :f  
-
-    form_builder.fields_for(method, options[:object], :child_index => 'NEW_RECORD') do |f|
-      render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })
+  
+  def add_child_link(name, association)
+    link_to(name, "javascript:void(0)", :class => "add_child", :"data-association" => association)
+  end
+  
+  def new_child_fields_template(form_builder, association, options = {})
+    options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
+    options[:partial] ||= association.to_s.singularize
+    options[:form_builder_local] ||= :f
+    
+    content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+      form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
+        render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+      end
     end
-  end
-
-  def generate_template(form_builder, method, options = {})
-    escape_javascript generate_html(form_builder, method, options)
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,44 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-replace_ids = function(s){
-  var new_id = new Date().getTime();
-  return s.replace(/NEW_RECORD/g, new_id);
-}
-
-var myrules = {
-  '.remove': function(e){
-    el = Event.findElement(e);
-    target = el.href.replace(/.*#/, '.')
-    el.up(target).hide();
-    if(hidden_input = el.previous("input[type=hidden]")) hidden_input.value = '1'
-  },
-  '.add_nested_item': function(e){
-    el = Event.findElement(e);
-    template = eval(el.href.replace(/.*#/, ''))
-    $(el.rel).insert({     
-      bottom: replace_ids(template)
-    });
-  },
-  '.add_nested_item_lvl2': function(e){
-    el = Event.findElement(e);
-    elements = el.rel.match(/(\w+)/g)
-    parent = '.'+elements[0]
-    child = '.'+elements[1]
-    
-    child_container = el.up(parent).down(child)    
-    parent_object_id = el.up(parent).down('input').name.match(/.*\[(\d+)\]/)[1]
-    
-    template = eval(el.href.replace(/.*#/, ''))
-
-    template = template.replace(/(attributes[_\]\[]+)\d+/g, "$1"+parent_object_id)
-    
-   // console.log(template)
-    child_container.insert({     
-      bottom: replace_ids(template)
-     });
-  }
-};
-
-Event.observe(window, 'load', function(){
-  $('container').delegate('click', myrules);
+$(function() {
+  $('form a.add_child').click(function() {
+    var assoc   = $(this).attr('data-association');
+    var content = $('#' + assoc + '_fields_template').html();
+    var regexp  = new RegExp('new_' + assoc, 'g');
+    var new_id  = new Date().getTime();
+        
+    $(this).parent().before(content.replace(regexp, new_id));    
+    return false;
+  });
+  
+  $('form a.remove_child').live('click', function() {
+    var hidden_field = $(this).prev('input[type=hidden]')[0];
+    if(hidden_field) {
+      hidden_field.value = '1';
+    }
+    $(this).parents('.fields').hide();
+    return false;
+  });
 });
