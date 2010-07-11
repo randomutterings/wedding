29e32f76399e0bbaa6c548a39af627d92b340e8c
Chris Barnes <randomutterings@gmail.com>
2010-07-11 14:46:28 -0400

    sorted events on rsvp form, added validation to number of attending

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,6 +4,5 @@ class Guest < ActiveRecord::Base
   has_many  :events, :through => :rsvps
   validates_presence_of :name, :email
   accepts_nested_attributes_for :rsvps, 
-                                :allow_destroy => true,
-                                :reject_if => proc { |attrs| attrs['attending'] == '0' || attrs['attending'].blank? }
+                                :allow_destroy => true
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,5 @@
 class Rsvp < ActiveRecord::Base
   belongs_to  :guest
   belongs_to  :event
+  validates_numericality_of :attending, :greater_than => 0, :message => " cannot be blank."
 end
