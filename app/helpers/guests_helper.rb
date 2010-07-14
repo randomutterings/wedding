40eba695392951b3fec17d53865729de08ca8cfe
Chris Barnes <randomutterings@gmail.com>
2010-07-13 22:26:39 -0400

    fixed problem where rsvp child template was submitted with the form, reference http://stackoverflow.com/questions/1704142/unobtrusive-dynamic-form-fields-in-rails-with-jquery

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -11,10 +11,12 @@ module GuestsHelper
     options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
     options[:partial] ||= association.to_s.singularize
     options[:form_builder_local] ||= :f
-    
-    content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
-      form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
-        render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+
+    content_for :jstemplates do
+      content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+        form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|        
+          render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })        
+        end
       end
     end
   end
